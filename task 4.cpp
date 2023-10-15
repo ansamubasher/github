dd0717b58d9acd9b98416d7d1bd3aca1f9949d7b
ansamubasher <147871690+ansamubasher@users.noreply.github.com>
2023-10-15 11:13:10 +0500

    first update

--- task 4.cpp
+++ task 4.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
 
-    for (int i=1; i<=3;i++){  
+    for (int i=1; i<=3;i++){  //loop 1
         for (int j=1; j<=3; j++){
             for (int k=1; k<=3; k++){
                 cout<<i<<j<<k<<endl;  //displays all the possible combinations
